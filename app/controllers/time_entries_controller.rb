4fd53cc907a2961771eaa19711e3993afe6ffcce
David Roetzel <david@roetzel.de>
2012-01-03 15:08:07 +0100

    More scaffolding for time entries.

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,10 +1,33 @@
 class TimeEntriesController < ApplicationController
 
   def index
+    @time_entries = TimeEntry.all
   end
 
   def new
-    @time_entry = TimeEntry.new
+    @time_entry = TimeEntry.new(:date => Date.today)
+  end
+
+  def create
+    @time_entry = TimeEntry.new(params[:time_entry])
+    if @time_entry.save
+      redirect_to time_entries_path, :notice => "Success!"
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @time_entry = TimeEntry.find(params[:id])
+  end
+
+  def update
+    @time_entry = TimeEntry.find(params[:id])
+    if @time_entry.update_attributes(params[:time_entry])
+      redirect_to time_entries_path, :notice => "Success!"
+    else
+      render :action => "edit"
+    end
   end
 
 end
